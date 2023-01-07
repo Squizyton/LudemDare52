42dc547e7240a67d49c2966772316439db5bbbd8
Joel <fallendemongod@gmail.com>
2023-01-07 11:52:37 -0500

    Switching Modes now available

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using UI;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
- 
+
+    //Managers
+    [Header("Managers")] 
+    [SerializeField]private UIManager uiManager;
+    [SerializeField] private CameraManager camManager;
+    
+    //MOde
     [Header("Current Mode")]
     public CurrentMode currentMode;
 
+    //Game Stats
     [Header("Game Stats")] 
     [SerializeField] private float timeAlive;
     [SerializeField] public int currentWave;
     [SerializeField] public int currentScore;
     [SerializeField] public int currentKills;
 
-    
+    //Timer Stats
     [Header("Timers")]
     [SerializeField]private float timeTillNextWave;
     
@@ -25,30 +33,32 @@ public class GameManager : MonoBehaviour
         if(Instance) Destroy(this); else Instance = this;
     }
 
-    // Update is called once per frame
+    // Update is called once per framez
     void Update()
     {
-        
+        switch (currentMode)
+        {
+            case CurrentMode.FPS:
+                //Increment total time alive
+                timeAlive += Time.deltaTime;
+                break;
+        }
     }
     
-    
-
     public void ChangeMode(CurrentMode newMode)
     {
         currentMode = newMode;
 
-        switch (newMode)
+        //Change the mode of the current mode based on the new mode
+        Cursor.lockState = newMode switch
         {
-            case CurrentMode.TopDown:
-                Cursor.lockState = CursorLockMode.None;
-                break;
-            case CurrentMode.FPS:
-                Cursor.lockState = CursorLockMode.Locked;
-                break;
-        }
-
+            CurrentMode.TopDown => CursorLockMode.None,
+            CurrentMode.FPS => CursorLockMode.Locked,
+            _ => Cursor.lockState
+        };
         
-        
+        //Tell Camera Manager to change the camera mode
+        camManager.ChangeMode(newMode);
     }
     public enum CurrentMode
     {
