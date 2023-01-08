642a283a0b28523db2f504ba905959fe7536c2cc
Chan <jan.dzyr2@gmail.com>
2023-01-08 14:59:25 +0100

    Cleaning in Audio + Barn Sounds and Cows

--- Assets/Scripts/Animation/EventReceiver.cs
+++ Assets/Scripts/Animation/EventReceiver.cs
@@ -4,39 +4,14 @@ using UnityEngine;
 
 public class EventReceiver : MonoBehaviour
 {
-
-    GameObject FoundObject;
-    string RaycastReturn;
-    RaycastHit hit;
-    float distance = 50f;
-
-
     public void FmodPostFootstepsEvent()
     {
-        CheckSurfaceMaterial();
+        //CheckSurfaceMaterial();
         PlaySound("event:/SFX/Enemies/Enemy_Greg/Enemy_Greg_FS");
     }
 
     public void PlaySound(string sound)
     {
         FMODUnity.RuntimeManager.PlayOneShotAttached(sound, gameObject);   //FMOD
-    }
-
-
-    private void CheckSurfaceMaterial()
-    {
-        if(Physics.Raycast(transform.position, Vector3.down, out hit, distance)) 
-        {
-            RaycastReturn = hit.collider.gameObject.name;
-            FoundObject = GameObject.Find(RaycastReturn);
-            if (FoundObject.TryGetComponent(out ImpactSoundMaterial soundMaterial))
-            {
-                soundMaterial.CheckMaterial(gameObject);
-            }
-            else
-            {
-                Debug.LogError($"Object {FoundObject} doesn't have <b>ImpactSoundMaterial</b> on self!");
-            }
-        }
     }
 }
