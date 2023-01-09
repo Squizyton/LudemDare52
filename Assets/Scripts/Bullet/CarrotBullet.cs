9fba93013e60dbadb2fee547e255ef658ec9fa7d
Vladimir Karashchuk <43484800+GOODSPIDERR@users.noreply.github.com>
2023-01-09 12:48:22 -0500

    i'm stuff

--- Assets/Scripts/Bullet/CarrotBullet.cs
+++ Assets/Scripts/Bullet/CarrotBullet.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 
 public class CarrotBullet : BaseBullet
 {
-   private int pierceCount;
-   override protected void OnHit(Transform hit)
-   {
-        //FMODUnity.RuntimeManager.PlayOneShotAttached("event:/Tests/gun hit", gameObject);   //FMOD impact test
-        pierceCount += 1;
-        hit.TryGetComponent(out BasicEnemy enemy);
-        
-        if(enemy)
-        {
-         enemy.OnHit(damage);   
-        }
-        if(pierceCount > 3) Destroy(gameObject);
-    }
+	private int pierceCount;
+	[SerializeField] private GameObject hitFX;
+	override protected void OnHit(Transform hit)
+	{
+		Instantiate(hitFX, transform.position, transform.rotation);
+		//FMODUnity.RuntimeManager.PlayOneShotAttached("event:/Tests/gun hit", gameObject);   //FMOD impact test
+		pierceCount += 1;
+		hit.TryGetComponent(out BasicEnemy enemy);
+
+		if (enemy)
+		{
+			enemy.OnHit(damage);
+
+
+		}
+		if (pierceCount > 3) Destroy(gameObject);
+	}
 }
--- Assets/Scripts/Bullet/PepperBullet.cs
+++ Assets/Scripts/Bullet/PepperBullet.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 
 public class PepperBullet : BaseBullet
 {
+	[SerializeField] private GameObject hitFX;
+	override protected void OnHit(Transform hit)
+	{
+		//FMODUnity.RuntimeManager.PlayOneShotAttached("event:/Tests/gun hit", gameObject);   //FMOD impact test
+		Instantiate(hitFX, transform.position, transform.rotation);
+		hit.TryGetComponent(out BasicEnemy enemy);
 
-   override protected void OnHit(Transform hit)
-   {
-        //FMODUnity.RuntimeManager.PlayOneShotAttached("event:/Tests/gun hit", gameObject);   //FMOD impact test
-        
-        hit.TryGetComponent(out BasicEnemy enemy);
-        
-        if(enemy)
-        {
-         enemy.OnHit(damage, true);   
-        }
-        
-        Destroy(gameObject);
-    }
+		if (enemy)
+		{
+			enemy.OnHit(damage, true);
+		}
+
+		Destroy(gameObject);
+	}
 }
