c901f05931ec040e980ef31ac769d5ed5e0fb1d6
Joel <fallendemongod@gmail.com>
2023-01-08 19:09:47 -0500

    Base gun fix

--- Assets/Scripts/Guns/BaseGun.cs
+++ Assets/Scripts/Guns/BaseGun.cs
@@ -8,35 +8,39 @@ using static UnityEngine.Rendering.DebugUI;
 
 namespace Guns
 {
-	public class BaseGun : MonoBehaviour
-	{
-		[Header("Important Things")] public Transform spawnPoint;
+    public class BaseGun : MonoBehaviour
+    {
+        [Header("Important Things")] public Transform spawnPoint;
 
-		[Header("Bullet Index")] public int currentBullet;
-		[Header("AmmoTypes")]
-		[SerializeField] public BaseBullet[] bulletList;
-
-
-		[Header("Base Stats")]
-		[SerializeField]
-		protected int maxAmmoPerClip;
-
-		[SerializeField] protected int currentMagazine;
-		[SerializeField] protected int ammoInSack;
-		[SerializeField] protected float reloadTime;
-		[SerializeField] protected float fireRate;
+        [Header("Bullet Index")] public int currentBullet;
+        [Header("AmmoTypes")]
+        [SerializeField] public BaseBullet[] bulletList;
 
 
-		[Header("Is Clauses")] private bool isReloading;
-		[SerializeField] protected bool isAutomatic;
-		[SerializeField] private bool hasInfiniteAmmo;
+        [Header("Base Stats")] [SerializeField]
+        protected int maxAmmoPerClip;
 
-		private bool canFire = true;
-		private float totalReloadTime;
-		private Vector3 hitPoint;
+        [SerializeField] protected int currentMagazine;
+        [SerializeField] protected int ammoInSack;
+        [SerializeField] protected float reloadTime;
+        [SerializeField] protected float fireRate;
 
-		private Animator animator;
 
+        [Header("Is Clauses")] private bool isReloading;
+        [SerializeField] protected bool isAutomatic;
+        [SerializeField] private bool hasInfiniteAmmo;
+
+        private bool canFire = true;
+        private float totalReloadTime;
+        private Vector3 hitPoint;
+
+
+        public LayerMask layerMask;
+        private void Start()
+        {
+         
+            SpecificGunStart();
+        }
 
         private void OnEnable()
         {
@@ -53,27 +57,23 @@ namespace Guns
                 Debug.Log("SpecialGun");
             }
 
-		private void OnEnable()
-		{
-			if (!isAutomatic)
-			{
-				FMODUnity.RuntimeManager.StudioSystem.setParameterByNameWithLabel("GunType", "BaseGun");
-				Debug.Log("BaseGun");
-			}
-			else
-			{
-				FMODUnity.RuntimeManager.StudioSystem.setParameterByNameWithLabel("GunType", "SpecialGun");
-				Debug.Log("SpecialGun");
-			}
+        }
 
-		}
+        //Called if you want the gun to do a specific thing on start
+        protected virtual void SpecificGunStart()
+        {
+            FeedStatsIntoGun(bulletList[currentBullet].GetBulletInfo());
+            currentMagazine = 15;
+        }
 
-		//Called if you want the gun to do a specific thing on start
-		protected virtual void SpecificGunStart()
-		{
-			FeedStatsIntoGun(bulletList[currentBullet].GetBulletInfo());
-			currentMagazine = 15;
-		}
+        //Use this to change the gun stats
+        //This is the base for the PEA SHOOTER. --------- Override this-------------
+        protected virtual void FeedStatsIntoGun(PlantInfo info) // Pea chooter ignores plantinfo because it can't change
+        {
+            
+            maxAmmoPerClip = bulletList[currentBullet].GetBulletInfo().maxClipSize;
+            fireRate = bulletList[currentBullet].GetBulletInfo().gunFireRate;
+        }
 
         public void SwapAmmo()
         {
@@ -93,12 +93,10 @@ namespace Guns
             }
         }
 
-			maxAmmoPerClip = bulletList[currentBullet].GetBulletInfo().maxClipSize;
-			fireRate = bulletList[currentBullet].GetBulletInfo().gunFireRate;
-		}
 
-		public void SwapAmmo()
-		{
+        public virtual void Shoot()
+        {
+            if (!canFire) return;
 
             if (currentMagazine > 0)
             {
@@ -110,23 +108,25 @@ namespace Guns
                 UIManager.Instance.UpdateAmmoCount(currentMagazine, ammoInSack, hasInfiniteAmmo);
                 //shoot a raycast from the middle of the screen
 
-		public virtual void Shoot()
-		{
-			if (!canFire) return;
 
-			if (currentMagazine > 0)
-			{
-				currentMagazine--;
-				canFire = false;
-				// Update ammo in inventory
-				PlantInfo currentAmmoType = bulletList[currentBullet].GetBulletInfo();
-				PlayerInventory.Instance.RemoveAmmo(currentAmmoType);
-				animator.SetTrigger("Shoot");
+                FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Player/Player_Gun");   //FMOD gun test
 
-				//shoot a raycast from the middle of the screen
+                //rotate the bullet to face the hit point
+                var position = spawnPoint.position;
+                var rotation = Quaternion.LookRotation(hitPoint - position);
+                //spawn the bullet
+                var bullet = Instantiate(bulletList[currentBullet].gameObject, position, rotation);
+               
 
+                if (IsAutomatic())
+                    StartCoroutine(CoolDown());
+            }
+            else
+            {
+                ReloadSequence(bulletList[currentBullet].GetBulletInfo().gunReloadTime);
+            }
+        }
 
-				FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Player/Player_Gun");   //FMOD gun test
 
         public void ReloadSequence(float timeToReload)
         {
@@ -155,107 +155,92 @@ namespace Guns
             
             
 
+            //If we hit something
+            //Store the hit point
+            if (Physics.Raycast(ray, out var hit, Mathf.Infinity, layerMask))
+            {
+                hitPoint = hit.point;
+            } //Developer's note: I wanna make this cleaner. It's NOT pretty
 
-		public void ReloadSequence(float timeToReload)
-		{
-			animator.SetTrigger("Reload");
-			UIManager.Instance.ReloadGroupStatus(true, timeToReload);
-			isReloading = true;
-			reloadTime = timeToReload;
-			totalReloadTime = 0;
-		}
+            #region Reloading
 
+            if (!isReloading) return;
+           
+            if (reloadTime > 0f)
+            {
+                reloadTime -= Time.deltaTime;
+                totalReloadTime += Time.deltaTime;
+                UIManager.Instance.FeedReloadTime(totalReloadTime);
+            }
+            else
+            {
+                if (!hasInfiniteAmmo)
+                {
+                    //get the difference between the current ammo and the max ammo
+                    var difference = maxAmmoPerClip - currentMagazine;
+                    PlantInfo currentAmmoType = bulletList[currentBullet].GetBulletInfo();
+                    //Set ammoInSack to inventory amount
+                    ammoInSack = PlayerInventory.Instance.GetAmmo(currentAmmoType) - currentMagazine;
 
-		private void Update()
-		{
+                    //If the player has enough ammo to reload
+                    if (ammoInSack >= maxAmmoPerClip)
+                    {
+                        ammoInSack -= difference;
+                        currentMagazine = maxAmmoPerClip;
+                    }
+                    else
+                    {
+                        currentMagazine = ammoInSack;
+                        ammoInSack = 0;
+                    }
+                }
+                else
+                {
+                    currentMagazine = maxAmmoPerClip;
+                }
 
                 isReloading = false;
                 UIManager.Instance.ReloadGroupStatus(false, 0);
                 UIManager.Instance.UpdateAmmoCount(currentMagazine, ammoInSack, hasInfiniteAmmo);
             }
 
+            #endregion
+        }
+
+        private IEnumerator CoolDown()
+        {
+            yield return new WaitForSeconds(fireRate);
+            canFire = true;
+        }
 
 
-			//If we hit something
-			//Store the hit point
-			if (Physics.Raycast(ray, out var hit, Mathf.Infinity, layerMask))
-			{
-				hitPoint = hit.point;
-			} //Developer's note: I wanna make this cleaner. It's NOT pretty
+        public bool IsAutomatic()
+        {
+            return isAutomatic;
+        }
 
-			#region Reloading
+        public bool IsReloading()
+        {
+            return isReloading;
+        }
 
-			if (!isReloading) return;
 
-			if (reloadTime > 0f)
-			{
-				reloadTime -= Time.deltaTime;
-				totalReloadTime += Time.deltaTime;
-				UIManager.Instance.FeedReloadTime(totalReloadTime);
-			}
-			else
-			{
-				if (!hasInfiniteAmmo)
-				{
-					//get the difference between the current ammo and the max ammo
-					var difference = maxAmmoPerClip - currentMagazine;
-					PlantInfo currentAmmoType = bulletList[currentBullet].GetBulletInfo();
-					//Set ammoInSack to inventory amount
-					ammoInSack = PlayerInventory.Instance.GetAmmo(currentAmmoType) - currentMagazine;
+        public void SetCanFire(bool value)
+        {
+            this.canFire = value;
+        }
 
-					//If the player has enough ammo to reload
-					if (ammoInSack >= maxAmmoPerClip)
-					{
-						ammoInSack -= difference;
-						currentMagazine = maxAmmoPerClip;
-					}
-					else
-					{
-						currentMagazine = ammoInSack;
-						ammoInSack = 0;
-					}
-				}
-				else
-				{
-					currentMagazine = maxAmmoPerClip;
-				}
 
-				isReloading = false;
-				UIManager.Instance.ReloadGroupStatus(false, 0);
-			}
-
-			#endregion
-		}
+        public bool CanFire()
+        {
+            return canFire;
+        }
 
         public bool IsMagFull()
         {
             return currentMagazine == maxAmmoPerClip;
         }
 
-
-		public bool IsAutomatic()
-		{
-			return isAutomatic;
-		}
-
-		public bool IsReloading()
-		{
-			return isReloading;
-		}
-
-
-		public void SetCanFire(bool value)
-		{
-			this.canFire = value;
-		}
-
-
-		public bool CanFire()
-		{
-			return canFire;
-		}
-
-
-
-	}
+      
+    }
 }
