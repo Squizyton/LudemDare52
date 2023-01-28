af839c2f0b53e0ecf804157dd84154defd343ff6
Joel <fallendemongod@gmail.com>
2023-01-27 23:44:09 -0500

    Connection to Steam now works

---
+++ Assets/Scripts/Steam Scripts/SteamScript.cs
@@ -0,0 +1,20 @@
+using System;
+
+using UnityEngine;
+using Steamworks;
+
+
+namespace Steam_Scripts
+{
+    public class SteamScript : MonoBehaviour
+    {
+        private void Start()
+        {
+            //Check to see if SteamManager is initialized
+            if (!SteamManager.Initialized) return;
+            
+            var steamName = SteamFriends.GetPersonaName();
+            Debug.Log(steamName);
+        }
+    }
+}
